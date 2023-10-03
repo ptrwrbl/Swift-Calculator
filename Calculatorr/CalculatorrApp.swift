4b667d557454bd2b0a711b259e7a8ecd086c3944
ptrwrbl <pirrowsky@gmail.com>
2023-10-03 17:12:18 +0200

    Initial Commit

---
+++ Calculatorr/CalculatorrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorrApp.swift
+//  Calculatorr
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
